2087b904284835048976e8dc8affeca82e520158
Chengyu Wang <cw521@texel03.doc.ic.ac.uk>
2021-11-14 19:59:44 +0000

    fix common output

--- common.cpp
+++ common.cpp
@@ -7,7 +7,7 @@ bool checkNumeric(const char* filename){
 	ifstream in;
 	in.open(filename);
 	if(in.fail()){
-		cout<<"error opening plugboard configuration file"<<endl;
+		cerr<<"error opening configuration file"<<endl;
 		exit(ERROR_OPENING_CONFIGURATION_FILE);
 	}
 	char ch;
@@ -18,7 +18,6 @@ bool checkNumeric(const char* filename){
 			continue;
 		}*/
 		if(ch>'9'||ch < '0'){
-			cout<<ch<<endl;
 			return false;
 		}
 		in>>ch;
